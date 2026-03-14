8b993f1ab86df465aaac26cea353f7812dd4b17a
501urchin <jaychhitu@gmail.com>
2026-03-14 22:28:32 +0100

    comment out unimpimented string funcs

--- internal/string/custom.go
+++ internal/string/custom.go
@@ -7,17 +7,13 @@ import (
 	gverrors "github.com/501urchin/gv/internal/errors"
 )
 
-func (s *StringValidator[T]) UUID() *StringValidator[T] {
-	return s
-}
-
 var emailReg = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
 
 func (s *StringValidator[T]) Email() *StringValidator[T] {
 	if s.err != nil {
 		return s
 	}
-	
+
 	if !emailReg.Match(gopt.StringToBytes(string(s.val))) {
 		s.err = gverrors.ErrEmail
 	}
@@ -25,10 +21,10 @@ func (s *StringValidator[T]) Email() *StringValidator[T] {
 	return s
 }
 
-func (s *StringValidator[T]) Hex() *StringValidator[T]    { return s }
-func (s *StringValidator[T]) URL() *StringValidator[T]    { return s }
-func (s *StringValidator[T]) URI() *StringValidator[T]    { return s }
-func (s *StringValidator[T]) Alpha() *StringValidator[T]  { return s }
-func (s *StringValidator[T]) Lower() *StringValidator[T]  { return s }
-func (s *StringValidator[T]) Upper() *StringValidator[T]  { return s }
-func (s *StringValidator[T]) Base64() *StringValidator[T] { return s }
+// func (s *StringValidator[T]) UUID() *StringValidator[T]   { return s }
+// func (s *StringValidator[T]) Hex() *StringValidator[T]    { return s }
+// func (s *StringValidator[T]) URL() *StringValidator[T]    { return s }
+// func (s *StringValidator[T]) Alpha() *StringValidator[T]  { return s }
+// func (s *StringValidator[T]) Lower() *StringValidator[T]  { return s }
+// func (s *StringValidator[T]) Upper() *StringValidator[T]  { return s }
+// func (s *StringValidator[T]) Base64() *StringValidator[T] { return s }
