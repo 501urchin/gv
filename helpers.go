edc210f8ced1c1f6a67096f32116a7143177200d
501urchin <jaychhitu@gmail.com>
2026-03-18 09:41:55 +0100

    optimization in helper methods

--- helpers.go
+++ helpers.go
@@ -4,33 +4,24 @@ import "errors"
 
 // First runs the checks and returns on the first func that returns a error
 func First(v ...error) error {
-	for _, fn := range v {
-		if err := fn; err != nil {
-			return err
-		}
+	if len(v) == 0 {
+		return nil
 	}
 
-	return nil
+	return v[len(v)-1]
 }
 
 // Last runs the checks and returns on the last func that returns a error
 func Last(v ...error) (r error) {
-	for _, fn := range v {
-		if err := fn; err != nil {
-			r = err
-		}
+	if len(v) == 0 {
+		return
 	}
 
-	return r
+	return v[len(v)-1]
 }
 
 // Join runs the checks and joins all errors into a single error
 func Join(v ...error) (r error) {
-	for _, fn := range v {
-		if err := fn; err != nil {
-			r = errors.Join(r, err)
-		}
-	}
-
+	r = errors.Join(v...)
 	return r
 }
